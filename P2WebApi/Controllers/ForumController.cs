c7e73ddf3f12becad9f8a30742aebb6df2e0cfa8
ccwilki1 <ccwilki2@yahoo.com>
2021-11-27 12:45:11 -0500

    controllers added Event, Forum, Invite

---
+++ P2WebApi/Controllers/ForumController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BL;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace P2WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ForumController : ControllerBase
+    {
+        //Dependency injection
+        private readonly IForumBL _ForumBL;
+
+        public ForumController(IForumBL p_ForumBL)
+        {
+            _ForumBL = p_ForumBL;
+        }
+
+        [HttpGet("{p_id}")]
+        public IActionResult GetForumById(int p_id)
+        {
+            return Ok(_ForumBL.GetForumById(p_id));
+        }
+
+
+         [HttpPost("Add")]
+        public IActionResult AddFourm([FromBody] Event p_fourm)
+        {
+            return Created("api/Forum/Add", _ForumBL.AddEvent(p_event));
+        }
+
+       
+        [HttpDelete("Delete")]
+        public IActionResult DeleteEvent([FromBody] Event p_event)
+        {
+            return Ok(_EventBL.DeleteEvent(p_event));
+        }
+
+    }
+}
