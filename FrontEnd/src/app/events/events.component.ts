db9023e257bc43d51abeddc5442566e6677d1cf6
JoshuaJL2021 <JoshuaJLopez@yahoo.com>
2021-11-29 12:41:57 -0500

    front end folder

---
+++ FrontEnd/src/app/events/events.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { News } from '../models/news';
+import { Users } from '../models/Users';
+import { Weather } from '../models/Weather';
+import { RevAPIService } from '../services/rev-api.service';
+
+@Component({
+  selector: 'app-events',
+  templateUrl: './events.component.html',
+  styleUrls: ['./events.component.css']
+})
+export class EventsComponent implements OnInit {
+
+  text2: string = "";
+  resultado: string = "";
+  listOfNewsArticles: News[] = [];
+  listOfWeatherForecast:Weather[]=[];
+  listOfUsers:Users[]=[];
+
+  constructor(private revApi: RevAPIService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+
+  getusers() {
+    this.revApi.Users().subscribe((response) => {
+      console.log(response);
+
+      //It will set the show property to false to each element and also add it to our listOfRest
+      response.forEach(element => {
+        
+        this.listOfUsers.push(element);
+      });
+    });
+      //It will set the show property to false to each element and also add it to our listOfRest
+
+
+
+  }
+
+}
+
