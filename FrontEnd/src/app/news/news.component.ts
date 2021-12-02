475e8a233cdc0e4f728d6fc764443a0089b15d83
JoshuaJL2021 <JoshuaJLopez@yahoo.com>
2021-12-01 21:41:06 -0500

    updated new

--- FrontEnd/src/app/news/news.component.ts
+++ FrontEnd/src/app/news/news.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { News } from '../models/news';
-import { Weather } from '../models/Weather';
 import { RevAPIService } from '../services/rev-api.service';
 
 @Component({
@@ -9,75 +8,66 @@ import { RevAPIService } from '../services/rev-api.service';
   templateUrl: './news.component.html',
   styleUrls: ['./news.component.css']
 })
-export class NewsComponent implements OnInit {
+
+export class NewsComponent implements OnInit
+{
   text2: string = "";
   resultado: string = "";
   listOfNewsArticles: News[] = [];
-  listOfWeatherForecast:Weather[]=[];
+  title: string="";
 
   constructor(private revApi: RevAPIService, private router: Router) { }
 
   ngOnInit()
   {
-      this.listOfNewsArticles=[];
-      this.revApi.GeoHeadlines().subscribe((response) => {
-        console.log(response);
-        //let pokemon2=JSON.parse(response);
-        //console.log(pokemon2);
-        for (var i in response.articles) {
-          let test: News = {
-            published: response.articles[i].published,
-            title: response.articles[i].title,
-            link: response.articles[i].link
-          };
-          this.listOfNewsArticles.push(test);
-        }
-      });
-    }
-
-  translate(p_text: string) {
-    this.text2 = p_text;
-    this.revApi.translate(this.text2)
-      .subscribe(data => {
-        this.resultado = data.data.translation;
-        console.log(data);
-      })
-  }
-
-  // News2() {
-  //   this.listOfNewsArticles=[];
-  //   this.revApi.GeoHeadlines().subscribe((response) => {
-  //     console.log(response);
-  //     //let pokemon2=JSON.parse(response);
-  //     //console.log(pokemon2);
-  //     for (var i in response.articles) {
-  //       let test: News = {
-  //         published: response.articles[i].published,
-  //         title: response.articles[i].title,
-  //         link: response.articles[i].link
-  //       };
-  //       this.listOfNewsArticles.push(test);
-  //     }
-  //   });
-  // }
-
-  JapaneseNews() {
-  this.listOfNewsArticles=[];
-    this.revApi.JapaneseHeadlines().subscribe((response) => {
+    this.listOfNewsArticles=[];
+    this.revApi.GeoHeadlines().subscribe((response) =>
+    {
       console.log(response);
-      //let pokemon2=JSON.parse(response);
-      //console.log(pokemon2);
-      for (var i in response.articles) {
-        let test: News = {
+      for (var i in response.articles)
+      {
+        let n: News =
+        {
           published: response.articles[i].published_date,
           title: response.articles[i].title,
           link: response.articles[i].link
         };
-        this.listOfNewsArticles.push(test);
+        this.listOfNewsArticles.push(n)
       }
-
+      this.listOfNewsArticles = this.listOfNewsArticles.slice(0,5);
     });
-
+    console.log(this.listOfNewsArticles);
   }
 
+  JapaneseNews()
+  {
+    this.listOfNewsArticles=[];
+    this.revApi.JapaneseHeadlines().subscribe((response) =>
+    {
+      console.log(response);
+      for (var i in response.articles)
+      {
+        let test: News =
+        {
+          published: response.articles[i].published_date,
+          title: response.articles[i].title,
+          link: response.articles[i].link
+        };
+        this.listOfNewsArticles.push(test)
+      }
+      this.listOfNewsArticles = this.listOfNewsArticles.slice(0,5);
+    });
+    console.log(this.listOfNewsArticles);
+  }
+
+  translate(p_text: string)
+  {
+    this.text2 = p_text;
+    this.revApi.translate(this.text2)
+      .subscribe(data =>
+      {
+        this.resultado = data.data.translation;
+        console.log(data);
+      })
+  }
 }
